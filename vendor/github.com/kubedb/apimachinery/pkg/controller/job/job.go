bc7f9a387f00ebd1ec8aeb5f474ac5892bca90c2
Tamal Saha <tamal@appscode.com>
2018-09-26 02:15:55 -0700

    Revendor api
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- vendor/github.com/kubedb/apimachinery/pkg/controller/job/job.go
+++ vendor/github.com/kubedb/apimachinery/pkg/controller/job/job.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/appscode/go/log"
+	"github.com/kubedb/apimachinery/apis"
 	api "github.com/kubedb/apimachinery/apis/kubedb/v1alpha1"
 	"github.com/kubedb/apimachinery/client/clientset/versioned/typed/kubedb/v1alpha1/util"
 	"github.com/kubedb/apimachinery/pkg/eventer"
@@ -52,7 +53,7 @@ func (c *Controller) handleBackupJob(job *batch.Job) error {
 				t := metav1.Now()
 				in.CompletionTime = &t
 				return in
-			}, api.EnableStatusSubresource); err != nil {
+			}, apis.EnableStatusSubresource); err != nil {
 				c.eventRecorder.Eventf(
 					snapshot,
 					core.EventTypeWarning,
@@ -122,27 +123,22 @@ func (c *Controller) handleRestoreJob(job *batch.Job) error {
 
 			var phase api.DatabasePhase
 			var reason string
+			objectMeta := metav1.ObjectMeta{Name: o.Name, Namespace: job.Namespace}
+
 			if jobSucceeded {
 				phase = api.DatabasePhaseRunning
+				if err := c.snapshotter.UpsertDatabaseAnnotation(objectMeta, map[string]string{
+					api.AnnotationInitialized: "",
+				}); err != nil {
+					return err
+				}
 			} else {
 				phase = api.DatabasePhaseFailed
 				reason = "Failed to complete initialization"
 			}
-			objectMeta := metav1.ObjectMeta{Name: o.Name, Namespace: job.Namespace}
-			err := c.snapshotter.SetDatabaseStatus(objectMeta, phase, reason)
-			if err != nil {
+			if err := c.snapshotter.SetDatabaseStatus(objectMeta, phase, reason); err != nil {
 				return err
 			}
-
-			if jobSucceeded {
-				err = c.snapshotter.UpsertDatabaseAnnotation(objectMeta, map[string]string{
-					api.AnnotationInitialized: "",
-				})
-				if err != nil {
-					return err
-				}
-			}
-
 			runtimeObj, err := c.snapshotter.GetDatabase(objectMeta)
 			if err != nil {
 				log.Errorln(err)
